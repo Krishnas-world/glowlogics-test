ede967f7c33a2e7760b88e282c788222061cb3e7
Krishnas-world <krishnapallan128@gmail.com>
2025-12-16 16:03:11 +0530

    changes: Initialized the repository

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="w-full bg-orange-600 p-4">
+      <h1 className="text-white text-2xl font-bold">GlowLogics</h1>
+    </header>
+  )
+}
